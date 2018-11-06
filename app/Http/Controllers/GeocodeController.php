62799d1c117f564c589eda46ac70e682b2fb2f11
Meet Godhani <meetgodhani23@gmail.com>
2018-11-05 22:08:49 -0500

    Fixed Cache

--- app/Http/Controllers/GeocodeController.php
+++ app/Http/Controllers/GeocodeController.php
@@ -16,27 +16,26 @@ class GeocodeController extends Controller
      */
     public function geocode(Request $request) {
       $cacheSlug = str_slug($request->get('address'));
-      $client = new Client([
-          'base_uri' => 'https://geocoder.api.here.com/6.2/',
-          'headers' => [
-              'Accept' => 'application/json'
-          ]
-      ]);
+      return Cache::rememberForever($cacheSlug, function () use ($request) {
+          $client = new Client([
+              'base_uri' => 'https://geocoder.api.here.com/6.2/',
+              'headers' => [
+                  'Accept' => 'application/json'
+              ]
+          ]);
 
-      $appId = env('GEO_APP_ID');
-      $appCode = env('GEO_APP_CODE');
-      $response = $client->request(
-        'GET',
-        'geocode.json',
-        [
-        'query' => [
-          'app_id' => $appId,
-          'app_code' => $appCode,
-          'searchText' => $request->get('address')
-        ]
-      ]);
-
-      return Cache::rememberForever($cacheSlug, function () use ($response) {
+          $appId = env('GEO_APP_ID');
+          $appCode = env('GEO_APP_CODE');
+          $response = $client->request(
+              'GET',
+              'geocode.json',
+              [
+                  'query' => [
+                      'app_id' => $appId,
+                      'app_code' => $appCode,
+                      'searchText' => $request->get('address')
+                  ]
+              ]);
           return json_decode($response->getBody(), true);
       });
     }
