978d17cb03ab9e45cfa5cae4fb6b35361498a477
Meet Godhani <meetgodhani23@gmail.com>
2018-12-06 00:19:30 -0500

    Fix

--- app/Http/Controllers/GeocodeController.php
+++ app/Http/Controllers/GeocodeController.php
@@ -11,24 +11,52 @@ class GeocodeController extends Controller
 
     public function license (Request $request)
     {
-        $client = new Client([
-            'base_uri' => 'https://api.gumroad.com/v2/',
-            'headers' => [
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/x-www-form-urlencoded'
-            ]
-        ]);
+        $curl = curl_init();
 
-        $response = $client->request(
-            'POST',
-            'license/verify',
-            [
-                'query' => [
-                    'product_permalink' => $request->get('product_permalink'),
-                    'license_key' => $request->get('license_key')
-                ]
-            ]);
-        return json_decode($response->getBody(), true);
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.gumroad.com/v2/licenses/verify?product_permalink=ridereceiptspro&license_key=C296393B-26A7415E-8FB656AD-D1BEDBDD",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "undefined=",
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/x-www-form-urlencoded",
+                "Postman-Token: 9dc42b3e-4bfe-4804-828d-3502dff83909",
+                "cache-control: no-cache"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+//        $client = new Client([
+//            'base_uri' => 'https://api.gumroad.com/v2/',
+//            'headers' => [
+//                'Accept' => 'application/json',
+//                'Content-Type' => 'application/x-www-form-urlencoded'
+//            ]
+//        ]);
+//
+//        $response = $client->request(
+//            'POST',
+//            'license/verify',
+//            [
+//                'query' => [
+//                    'product_permalink' => $request->get('product_permalink'),
+//                    'license_key' => $request->get('license_key')
+//                ]
+//            ]);
+//        return json_decode($response->getBody(), true);
     }
     /**
      * Login using Google
